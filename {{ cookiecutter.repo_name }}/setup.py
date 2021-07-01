5b3ae2ddd10f168fa1124bf5ddd0a219fe23f9e3
John Miller <millerji@ornl.gov>
2021-07-01 07:39:00 -0400

    Update setup.py to match example from python docs
    Example setup.py:
    https://packaging.python.org/tutorials/packaging-projects/

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -1,10 +1,42 @@
-from setuptools import find_packages, setup
+# from setuptools import find_packages, setup
+#
+# setup(
+#     name='src',
+#     packages=find_packages(),
+#     version='0.1.0',
+#     author='{{ cookiecutter.author_name }}',
+#     license='{% if cookiecutter.open_source_license == 'MIT' %}MIT{% elif cookiecutter.open_source_license == 'BSD-3-Clause' %}BSD-3{% endif %}',
+# )
 
-setup(
-    name='src',
-    packages=find_packages(),
-    version='0.1.0',
-    description='{{ cookiecutter.description }}',
+# The below is copied from:
+# https://packaging.python.org/tutorials/packaging-projects/
+# I used the variables from above to fill in the template below, but in some
+# cases where I didn't know the correct variable (or if it's set in
+# cookiecutter), I filled in what I thought made sense (eg, '.github_url').
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="{{ cookiecutter.project_name }}",
+    version="0.0.1",
     author='{{ cookiecutter.author_name }}',
-    license='{% if cookiecutter.open_source_license == 'MIT' %}MIT{% elif cookiecutter.open_source_license == 'BSD-3-Clause' %}BSD-3{% endif %}',
+    author_email="{{ cookiecutter.author_email }}",
+    description='{{ cookiecutter.description }}',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='{{ cookiecutter.github_url }}',
+    project_urls={
+        "Bug Tracker": "{{ cookiecutter.github_url }}/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: {% if cookiecutter.open_source_license == 'MIT' %}MIT{% elif cookiecutter.open_source_license == 'BSD-3-Clause' %}BSD-3{% endif %} License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
 )
