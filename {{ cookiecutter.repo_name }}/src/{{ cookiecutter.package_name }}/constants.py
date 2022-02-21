739cd6a7ac51cdee98827c127eee079e85c49564
John Miller <millerji@ornl.gov>
2022-02-21 09:39:57 -0500

    Change PROJECTDIR to PROJECT_ROOT

--- {{ cookiecutter.repo_name }}/src/{{ cookiecutter.package_name }}/constants.py
+++ {{ cookiecutter.repo_name }}/src/{{ cookiecutter.package_name }}/constants.py
@@ -1,11 +1,15 @@
 import os
+import {{cookiecutter.package_name}}
 
-PROJECTDIR = os.path.realpath(os.path.expanduser('~/projects/{{cookiecutter.repo_name}}'))
-assert os.path.exists(PROJECTDIR), 'PROJECTDIR does not exist: {}'.format(PROJECTDIR)
+# This assumes that the package has been installed, eg: `pip install -e .`.
+# print({{cookiecutter.package_name}}.__file__)  # /abspath/to/{{cookiecutter.repo_name}}/src/{{cookiecutter.package_name}}/__init__.py
 
-DATADIR = os.path.join(PROJECTDIR, 'data')
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname({{cookiecutter.package_name}}.__file__)))
+assert os.path.exists(PROJECT_ROOT), 'PROJECT_ROOT does not exist: {}'.format(PROJECT_ROOT)
+
+DATADIR = os.path.join(PROJECT_ROOT, 'data')
 assert os.path.exists(DATADIR), 'DATADIR does not exist: {}'.format(DATADIR)
 
-# CACHE = os.path.join(PROJECTDIR, '_cache')
+CACHE = os.path.join(PROJECT_ROOT, '_cache')
 
 # END.
